2f7e048f95ffdbadf25cb05919b3b77eafd29cd8
aramgrigoryan <aramgrigoryan@protonmail.ch>
2017-08-11 17:40:31 +0200

    fix readme file type to md

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_meta(meta):
 
 def get_long_description():
     """Get the long_description from the README.rst file. Assume UTF-8 encoding."""
-    with codecs.open(os.path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+    with codecs.open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
         long_description = f.read()
     return long_description
 
