258490e50d888cdced224c13d7792b9b66778fb5
aramgrigoryan <aramgrigoryan@protonmail.ch>
2017-08-18 16:02:39 +0200

    update cli for writing a bel script

--- src/bio2bel_ec/cli.py
+++ src/bio2bel_ec/cli.py
@@ -3,15 +3,20 @@ import sys
 import click
 
 from .tree import write_expasy_tree
+from .query import write_gene_ec_mapping
 
 @click.group()
 def main():
     """Tools for writing EC"""
 
 @main.command()
-@click.option('-o', '--output', type=click.File('w'), default=sys.stdout)
-def write(output):
-    write_expasy_tree(output)
+@click.option('-e', '--eoutput', type=click.File('w'), default=sys.stdout)
+def write(eoutput):
+    write_expasy_tree(eoutput)
 
+@main.command()
+@click.option('-bo', '--boutput', type=click.File('w'), default=sys.stdout)
+def write_bel(boutput):
+    write_gene_ec_mapping(boutput)
 if __name__ == '__main__':
     main()
