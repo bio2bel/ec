1bbe575addfe9e4b169d426d01e9ef5e38520019
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-08-20 09:34:29 +0200

    Externalize constants

--- tests/test_enrich.py
+++ tests/test_enrich.py
@@ -7,36 +7,40 @@ from pybel import BELGraph
 from pybel.constants import PROTEIN
 
 from bio2bel_ec.enrich import enrich_enzyme_classes
+ec = '1.14.99.1'
+ec_p = '1.14.99.-'
+ec_pp = '1.14. -.-'
+ec_ppp = '1. -. -.-'
+
+cyclooxygenase = PROTEIN, 'HGNC', 'PTGS2'
+cyclooxygenase_ec = PROTEIN, 'EC', ec
+cyclooxygenase_ec_p = PROTEIN, 'EC', ec_p
+cyclooxygenase_ec_pp = PROTEIN, 'EC', ec_pp
+cyclooxygenase_ec_ppp = PROTEIN, 'EC', ec_ppp
+
 
 
 @unittest.skipIf('CI' in os.environ, "Don't have PyUniProt data on Travis")
 class TestEnrich(unittest.TestCase):
-    def setUp(self):
-        self.cyclooxygenase = PROTEIN, 'HGNC', 'PTGS2'
-        self.cyclooxygenase_ec = PROTEIN, 'EC', '1.14.99.1'
-        self.cyclooxygenase_ec_p = PROTEIN, 'EC', '1.14.99.-'
-        self.cyclooxygenase_ec_pp = PROTEIN, 'EC', '1.14.-.-'
-        self.cyclooxygenase_ec_ppp = PROTEIN, 'EC', '1.-.-.-'
-
     def test_enrich(self):
         """Tests that the connection from the protein to the actual enzyme class is made and also
         the entire class hierarchy is ensured"""
         graph = BELGraph()
-        graph.add_simple_node(*self.cyclooxygenase)
+        graph.add_simple_node(*cyclooxygenase)
 
         self.assertEqual(1, graph.number_of_nodes())
 
         enrich_enzyme_classes(graph)
 
-        self.assertIn(self.cyclooxygenase_ec, graph)
-        self.assertIn(self.cyclooxygenase_ec_p, graph)
-        self.assertIn(self.cyclooxygenase_ec_pp, graph)
-        self.assertIn(self.cyclooxygenase_ec_ppp, graph)
+        self.assertIn(cyclooxygenase_ec, graph)
+        self.assertIn(cyclooxygenase_ec_p, graph)
+        self.assertIn(cyclooxygenase_ec_pp, graph)
+        self.assertIn(cyclooxygenase_ec_ppp, graph)
 
-        self.assertIn(self.cyclooxygenase_ec, graph.edge[self.cyclooxygenase])
-        self.assertIn(self.cyclooxygenase_ec_p, graph.edge[self.cyclooxygenase_ec])
-        self.assertIn(self.cyclooxygenase_ec_pp, graph.edge[self.cyclooxygenase_ec_p])
-        self.assertIn(self.cyclooxygenase_ec_ppp, graph.edge[self.cyclooxygenase_ec_pp])
+        self.assertIn(cyclooxygenase_ec, graph.edge[cyclooxygenase])
+        self.assertIn(cyclooxygenase_ec_p, graph.edge[cyclooxygenase_ec])
+        self.assertIn(cyclooxygenase_ec_pp, graph.edge[cyclooxygenase_ec_p])
+        self.assertIn(cyclooxygenase_ec_ppp, graph.edge[cyclooxygenase_ec_pp])
 
 
 if __name__ == '__main__':
