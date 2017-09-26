ee428f0585de32b52db95b9e077de6d46a2ca6a2
Aram Grigoryan <aram.grigoryan@scai.fraunhofer.de>
2017-09-26 20:16:42 +0200

    hot fixes

--- src/bio2bel_ec/enzyme.py
+++ src/bio2bel_ec/enzyme.py
@@ -7,12 +7,14 @@ log = logging.getLogger(__name__)
 log.setLevel(20)
 logging.basicConfig(level=20)
 
-from bio2bel_ec.constants import ENZCLASS_DATA_FILE, EC_PATTERN_REGEX, EC_DELETED_REGEX, EC_TRANSFERRED_REGEX, EC_PROSITE_REGEX
+from bio2bel_ec.constants import ENZCLASS_DATA_FILE, EC_PATTERN_REGEX, EC_DELETED_REGEX, EC_TRANSFERRED_REGEX, \
+    EC_PROSITE_REGEX
 from bio2bel_ec.tree import download_ec_data
 
 __all__ = [
     'expasy_parser',
 ]
+
 
 def expasy_parser():
     """
@@ -31,7 +33,7 @@ def expasy_parser():
             if descriptor == "//":
                 if ec_data_entry['ID'] != '':
                     expasy_db.append(ec_data_entry)
-                    #log.info(" EC_ENTRY: {}".format(ec_data_entry))
+                    # log.info(" EC_ENTRY: {}".format(ec_data_entry))
                 ec_data_entry = {
                     'ID': '',
                     'DE': '',
@@ -49,7 +51,7 @@ def expasy_parser():
                 log.info(" SKIPPING: {}".format(line.strip()))
                 continue
 
-            #parsing
+            # parsing
             if descriptor == 'ID':
                 ec_data_entry['ID'] = re.search(EC_PATTERN_REGEX, line).group()
                 continue
@@ -72,11 +74,9 @@ def expasy_parser():
                 ec_data_entry['PR'].append(re.search(EC_PROSITE_REGEX, line).group())
             elif descriptor == 'DR':
                 for dr_tuple in line[5:-2].split(' ;  '):
-                    ec_data_entry['DR'].append({'AC_Nb': dr_tuple.split(', ')[0], 'Entry_name': dr_tuple.split(', ')[1]})
+                    ec_data_entry['DR'].append(
+                        {'AC_Nb': dr_tuple.split(', ')[0], 'Entry_name': dr_tuple.split(', ')[1]})
             else:
                 log.warning(" Unknown Descriptor is found. Risk of missed data or corrupt/wrong file.")
 
     return expasy_db
-
-
-
