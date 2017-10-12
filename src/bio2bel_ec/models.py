400e06525c5187ef089ee11c5976b771f7ea9616
Aram Grigoryan <aram.grigoryan@scai.fraunhofer.de>
2017-10-12 14:25:41 +0200

    adding database model skeleton

--- src/bio2bel_ec/models.py
+++ src/bio2bel_ec/models.py
@@ -5,19 +5,73 @@ from sqlalchemy import Column, String, Integer, Table, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-TABLE_PREFIX = 'expasy'
-ENTRY_TABLE_NAME = '{}_entry'.format(TABLE_PREFIX)
-TREE_TABLE_NAME = '{}_tree'.format(TABLE_PREFIX)
+EC_TABLE_PREFIX = 'expasy'
+EC_ENTRY_TABLE_NAME = '{}_entry'.format(EC_TABLE_PREFIX)
+EC_TREE_TABLE_NAME = '{}_tree'.format(EC_TABLE_PREFIX)
+
+PROSITE_TABLE_PREFIX = 'prosite'
+PROSITE_ENTRY_TABLE_NAME = '{}_entry'.format(PROSITE_TABLE_PREFIX)
+PROSITE_TREE_TABLE_NAME = '{}_tree'.format(PROSITE_TABLE_PREFIX)
+
+DR_TABLE_PREFIX = 'uniprot-swissprot'
+DR_ENTRY_TABLE_NAME = '{}_entry'.format(DR_TABLE_PREFIX)
+DR_TREE_TABLE_NAME = '{}_tree'.format(DR_TABLE_PREFIX)
+
 
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 ec_hierarchy = Table(
-    TREE_TABLE_NAME,
+    EC_TREE_TABLE_NAME,
     Base.metadata,
-    Column('ec_id', String, ForeignKey('{}.id'.format(ENTRY_TABLE_NAME)), primary_key=True),
-    Column('description', String, ForeignKey('{}.id'.format(ENTRY_TABLE_NAME)), primary_key=False),
-    Column('prosite_table_id', String, ForeignKey('{}.id'.format(ENTRY_TABLE_NAME)), primary_key=False, ForeignKey=True),
-    Column('prot_table_id', String, ForeignKey('{}.id'.format(ENTRY_TABLE_NAME)), primary_key=False, ForeignKey=True),
+    Column('ec_id', String, ForeignKey('{}.id'.format(EC_ENTRY_TABLE_NAME)), primary_key=True),
+    Column('de', String, ForeignKey('{}.id'.format(EC_ENTRY_TABLE_NAME)), primary_key=False),
 )
+
+prosite_hierarchy = Table(
+    PROSITE_TREE_TABLE_NAME,
+    Base.metadata,
+    Column('ec_id', String, '{}.id'.format(EC_ENTRY_TABLE_NAME), foreign_key=True),
+    Column('pr_id', String, '{}.id'.format(PROSITE_ENTRY_TABLE_NAME), primary_key=True),
+)
+
+dr_hierarchy = Table(
+    DR_TREE_TABLE_NAME,
+    Base.metadata,
+    Column('id', Integer, primary_key=True),
+    Column('ec_id', String, '{}.id'.format(EC_ENTRY_TABLE_NAME), foreign_key=True),
+    Column('AC_Nb', String, '{}.id'.format(DR_ENTRY_TABLE_NAME)),
+    Column('Entry_name', String, '{}.id'.format(DR_ENTRY_TABLE_NAME))
+)
+
+class EC_Entry(Base):
+    """ExPASy's main entry"""
+    __tablename__ = EC_ENTRY_TABLE_NAME
+
+    id = Column(String(255))
+
+    de = Column(String(255))
+
+    children = relationship(
+        'EC_Entry',
+        secondary=ec_hierarchy,
+        primaryjoin=(id == ec_hierarchy.c.ec_id),
+        secondaryjoin=(id == ec_hierarchy.c.de)
+    )
+
+class Prosite_Entry(Base):
+    """Maps ec to prosite entries"""
+    __tablename__ = PROSITE_ENTRY_TABLE_NAME
+
+    id = Column(String(255))
+    ec_id = Column(String(255),foreignkey=True)
+
+class PR_Entry(Base):
+    """Maps ec to swissprot or uniprot"""
+    __tablename__ = DR_ENTRY_TABLE_NAME
+
+    id = Column(Integer)
+    ec_id = Column(String(255))
+    AC_Nb = Column(String(255))
+    Entry_name = Column(String(255))
