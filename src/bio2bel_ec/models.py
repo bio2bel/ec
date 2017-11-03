2f024b6659809e138c33f4481583bbbb4c98bef3
Aram Grigoryan <aram.grigoryan@scai.fraunhofer.de>
2017-11-03 15:18:08 +0100

    sqlalchemy skeleton heavy fixes after talking with Charlie

--- src/bio2bel_ec/models.py
+++ src/bio2bel_ec/models.py
@@ -22,79 +22,52 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-ec_hierarchy = Table(
+enzyme_hierarchy = Table(
     EC_TREE_TABLE_NAME,
     Base.metadata,
-    Column('ec_id', String, ForeignKey('{}.id'.format(EC_ENTRY_TABLE_NAME)), primary_key=True),
-    Column('de', String, ForeignKey('{}.id'.format(EC_ENTRY_TABLE_NAME)), primary_key=False),
+    Column('parent_id', Integer, ForeignKey('{}.id'.format(EC_ENTRY_TABLE_NAME)), primary_key=True),
+    Column('child_id', Integer, ForeignKey('{}.id'.format(EC_ENTRY_TABLE_NAME)), primary_key=True),
 )
 
-pr_hierarchy = Table(
-    PROSITE_TREE_TABLE_NAME,
-    Base.metadata,
-    Column('ec_id', String, '{}.id'.format(EC_ENTRY_TABLE_NAME), foreign_key=True),
-    Column('pr_id', String, '{}.id'.format(PROSITE_ENTRY_TABLE_NAME), primary_key=True),
-)
-
-dr_hierarchy = Table(
-    DR_TREE_TABLE_NAME,
-    Base.metadata,
-    Column('id', Integer, primary_key=True),
-    Column('ec_id', String, '{}.id'.format(EC_ENTRY_TABLE_NAME), foreign_key=True),
-    Column('AC_Nb', String, '{}.id'.format(DR_ENTRY_TABLE_NAME)),
-    Column('Entry_name', String, '{}.id'.format(DR_ENTRY_TABLE_NAME)),
-    Column('comment', String)
-)
-
-class EC_Entry(Base):
+class Enzyme_Entry(Base):
     """ExPASy's main entry"""
     __tablename__ = EC_ENTRY_TABLE_NAME
 
-    id = Column(String(255))
-
-    de = Column(String(255))
+    id = Column(Integer, primary_key=True)
+    enzyme_id = Column(String(255), secondary_key=True)
+    description = Column(String(255))
 
     children = relationship(
-        'EC_Entry',
-        secondary=ec_hierarchy,
-        primaryjoin=(id == ec_hierarchy.c.ec_id),
-        secondaryjoin=(de == ec_hierarchy.c.de)
+        'Enzyme_Entry',
+        secondary=enzyme_hierarchy,
+        primaryjoin=(id == enzyme_hierarchy.c.parent_id),
+        secondaryjoin=(id == enzyme_hierarchy.c.child_id)
     )
 
-class PR_Entry(Base):
+class Prosite_Entry(Base):
     """Maps ec to prosite entries"""
     __tablename__ = PROSITE_ENTRY_TABLE_NAME
 
-    id = Column(String(255))
-    ec_id = Column(String(255),foreignkey=True)
+    id=Column(Integer, primary_key=True)
+    prosite_id = Column(String(255), secondary_key=True)
+    enzyme_id = Column(String(255),foreignkey=True)
 
-    children = relationship(
-        'PR_Entry',
-        secondary=pr_hierarchy,
-        primaryjoin=(id==pr_hierarchy.c.pr_id),
-        secondaryjoin=(ec_id==pr_hierarchy.c.ec_id)
-    )
     ec = relationship(
-        EC_Entry,
+        Enzyme_Entry,
         backref=backref(PROSITE_ENTRY_TABLE_NAME, uselist=True)
     )
 
-class DR_Entry(Base):
+class Protein_Entry(Base):
     """Maps ec to swissprot or uniprot"""
     __tablename__ = DR_ENTRY_TABLE_NAME
 
-    id = Column(Integer)
-    ec_id = Column(String(255))
+    id = Column(Integer, primary_key=True)
+    enzyme_id = Column(String(255), foreignkey=True)
     AC_Nb = Column(String(255))
     Entry_name = Column(String(255))
     comment = Column(String(255)) #used to indicate UniProt or SWISSProt
-    children = relationship(
-        'DR_Entry',
-        secondary=dr_hierarchy,
-        primaryjoin=(id==dr_hierarchy.c.id),
-        secondaryjoin=(ec_id==dr_hierarchy.c.ec_id)
-    )
+
     ec = relationship(
-        EC_Entry,
+        Enzyme_Entry,
         backref=backref(DR_ENTRY_TABLE_NAME, uselist=True)
     )
