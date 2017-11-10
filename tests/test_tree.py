545acdaf90ecfa5ae87965043c3eeda9af8b2ef7
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-10 17:57:23 +0100

    Update test documentation

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -17,10 +17,11 @@ class TestTree(unittest.TestCase):
             self.graph = tree.populate_tree()
 
     def test_length(self):
+        """Assert that the number of nodes present in the test file is what we expect"""
         self.assertEqual(33, self.graph.number_of_nodes())
 
-    def test_1(self):
-        """checks some nodes"""
+    def test_node_existence(self):
+        """Assert that the given nodes and their parent relationships are present"""
         self.assertIn(tree.standard_ec_id('5. 4. 2.-'), self.graph.edge[tree.standard_ec_id('5. 4. -.-')])
 
         self.assertIn(tree.standard_ec_id('6. 6. 1.-'), self.graph.edge[tree.standard_ec_id('6. 6. -.-')])
