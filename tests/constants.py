d38df0f452dfc16a3b589a9510fd0196379ad213
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-10 17:54:36 +0100

    Remove unused constants

--- tests/constants.py
+++ tests/constants.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from pybel.constants import PYBEL_DATA_DIR
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 ENZCLASS_DATA_TEST_FILE = os.path.join(dir_path, 'enzyme_test.dat')
-
-ENZCLASS_DATA_DIR = os.path.join(PYBEL_DATA_DIR, 'expasy')
-ENZCLASS_DATABASE_NAME = 'enzyme_dat_test.sqlite'
-ENZCLASS_SQLITE_PATH = 'sqlite:///' + os.path.join(ENZCLASS_DATA_DIR, ENZCLASS_DATABASE_NAME)
