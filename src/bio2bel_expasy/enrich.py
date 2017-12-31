4ec67a2d6875e6c34fd7a612809d52fa910330c1
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-12-31 16:06:20 +0100

    Remove unused enrichment functions

--- src/bio2bel_expasy/enrich.py
+++ src/bio2bel_expasy/enrich.py
@@ -2,21 +2,19 @@
 
 import logging
 
-from pybel.constants import IS_A, NAME, PROTEIN
-from .constants import EXPASY
 from .manager import Manager
-from .utils import check_namespaces
 
 log = logging.getLogger(__name__)
 
 __all__ = [
     'enrich_proteins',
     'enrich_prosite_classes',
+    'enrich_enzymes'
 ]
 
 
 def enrich_proteins(graph, connection=None):
-    """Enriches proteins in the BEL graph with IS_A relations to their enzyme classes.
+    """Enriches proteins in the BEL graph with :data:`pybel.constants.IS_A` relations to their enzyme classes.
 
     1. Gets a list of UniProt proteins
     2. Annotates :data:`pybel.constants.IS_A` relations for all enzyme classes it finds
@@ -29,7 +27,7 @@ def enrich_proteins(graph, connection=None):
 
 
 def enrich_prosite_classes(graph, connection=None):
-    """enriches Enzyme classes for ProSite in the graph.
+    """Enriches Enzyme classes for ProSite nodes in the graph.
 
     :param pybel.BELGraph graph: A BEL graph
     :type connection: str or bio2bel_expasy.Manager
@@ -38,44 +36,11 @@ def enrich_prosite_classes(graph, connection=None):
     m.enrich_prosite_classes(graph)
 
 
-def enrich_parents_classes(graph, connection=None):
-    """Enriches graph nodes with parents.
-
-    :param pybel.BELGraph graph: A BEL graph
-    :param connection:
-    """
-    m = Manager.ensure(connection=connection)
-
-    for node, data in graph.nodes(data=True):
-        if not check_namespaces(data, PROTEIN, EXPASY):
-            continue
-        parent = m.get_parent(data[NAME])
-        if not parent:
-            log.warning('enrich_parents_classes(): Unable to find node %s', data[NAME])
-            continue
-
-        while parent:
-            parent_tuple = graph.add_node_from_data(parent.serialize_to_bel())
-            graph.add_unqualified_edge(node, parent_tuple, IS_A)
-            parent = m.get_parent(parent_tuple[2])
-
-
-def enrich_children_classes(graph, connection=None):
-    """Enriches graph nodes with children.
+def enrich_enzymes(graph, connection=None):
+    """Add all children of entries (enzyme codes with 4 numbers in them that can be directly annotated to proteins)
 
     :param pybel.BELGraph graph: A BEL graph
     :type connection: str or bio2bel_expasy.Manager
     """
     m = Manager.ensure(connection=connection)
-
-    for node, data in graph.nodes(data=True):
-        if not check_namespaces(data, PROTEIN, EXPASY):
-            continue
-        children = m.get_children(data[NAME])
-        if not children:
-            log.warning("enrich_children_classes(): Unable to find node %s", data[NAME])
-            continue
-
-        for child in children:
-            children_tuple = graph.add_node_from_data(child.serialize_to_bel())
-            graph.add_unqualified_edge(node, children_tuple, IS_A)
+    m.enrich_enzymes(graph)
