e024f56cd41933bc984b7c2daa83926f15320872
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-19 08:07:09 +0100

    Update CLI
    Use context to avoid repetitive code

--- src/bio2bel_expasy/cli.py
+++ src/bio2bel_expasy/cli.py
@@ -8,39 +8,41 @@ from .constants import DEFAULT_CACHE_CONNECTION
 from .manager import Manager
 
 
+
 @click.group()
-def main():
+@click.option('-c', '--connection', help='Defaults to {}'.format(DEFAULT_CACHE_CONNECTION))
+@click.pass_context
+def main(ctx, connection):
     """ExPASy to BEL"""
-    logging.basicConfig(level=10)
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    ctx.obj = Manager(connection=connection)
 
 
 @main.command()
-@click.option('-c', '--connection', help='Defaults to {}'.format(DEFAULT_CACHE_CONNECTION))
-def populate(connection):
+@click.pass_obj
+def populate(manager):
     """Populates the database"""
-    m = Manager(connection=connection)
-    m.populate()
+    manager.populate()
 
 
 @main.command()
 @click.option('-y', '--yes', is_flag=True)
-@click.option('-c', '--connection', help='Defaults to {}'.format(DEFAULT_CACHE_CONNECTION))
-def drop(yes, connection):
+@click.pass_obj
+def drop(yes, manager):
     """Drops the database"""
     if yes or click.confirm('Drop database?'):
-        m = Manager(connection=connection)
-        m.drop_all()
+        manager.drop_all()
 
 
 @main.command()
-@click.option('-c', '--connection', help='Defaults to {}'.format(DEFAULT_CACHE_CONNECTION))
 @click.option('-v', '--debug', is_flag=True)
 @click.option('-p', '--port')
 @click.option('-h', '--host')
-def web(connection, debug, port, host):
+@click.pass_obj
+def web(manager, debug, port, host):
     """Run the web app"""
-    from .web import create_app
-    app = create_app(connection=connection, url='/')
+    from .web import get_app
+    app = get_app(connection=manager, url='/')
     app.run(host=host, port=port, debug=debug)
 
 
