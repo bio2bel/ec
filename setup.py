93d30a6051da5dd09064f55dbfedb29dfb14824c
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-05-23 11:07:34 -0400

    Remove deployment extra from requirements

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ CLASSIFIERS = [
     'Topic :: Scientific/Engineering :: Bio-Informatics'
 ]
 INSTALL_REQUIRES = [
-    'pybel[deployment]>=0.11.1',
+    'pybel>=0.11.1',
     'requests',
     'tqdm',
     'sqlalchemy',
